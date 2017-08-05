5edbd545188938c26183120be4fe021fff01297b
‘SunFuGithub’ <2569768497@qq.com>
2017-08-05 15:31:39 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcoo/tcmp056/Test01.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcoo.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by iFunk on 2017/8/5.
+ */
+public class Test01 {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("孙福的第三次实验！");
+    }
+}
